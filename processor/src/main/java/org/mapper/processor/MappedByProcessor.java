675cd56e8498efc791f3eb4e64cedea4be8c567b
daolena-a <adrien.daolena@gmail.com>
2012-02-14 10:40:44 +0100

    generating package import and class declaration

--- processor/src/main/java/org/mapper/processor/MappedByProcessor.java
+++ processor/src/main/java/org/mapper/processor/MappedByProcessor.java
@@ -10,6 +10,7 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.io.File;
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.util.Set;
 
@@ -30,12 +31,28 @@ public class MappedByProcessor extends AbstractProcessor {
                     System.out.println("name"+clazz.getCanonicalName());
                     anno =(MappedBy) clazz.getAnnotation(MappedBy.class);
                     if(anno == null) break;
-                    File mappingFile = new File(clazz.getSimpleName()+"To"+anno.targetedClass()+".java");
-                    mappingFile.createNewFile();
-                    System.out.println("tse"+mappingFile.isFile());
-                    for (Field f : clazz.getFields()){
-                        
+                    File mappingFile = new File(clazz.getSimpleName()+"To"+anno.targetedClass().getSimpleName()+".java");
+                    if(mappingFile.exists()){
+                        mappingFile.delete();
                     }
+                    mappingFile.createNewFile();
+                    PrintWriter writer = new PrintWriter(mappingFile);
+                    writer.println("package org.mapper.gen");
+                    writer.println();
+                    writer.println("import "+clazz.getCanonicalName()+";");
+                    writer.println("import "+anno.targetedClass()+";");
+                    writer.println("public class "+clazz.getSimpleName()+"To"+anno.targetedClass().getSimpleName()+"{");
+                    for (Field f : clazz.getFields()){
+
+                    }
+                    writer.println("}");
+                    writer.flush();
+                    //package
+                    //import
+                    //className
+                    //interface?
+                    System.out.println("tse"+mappingFile.isFile());
+
                     System.out.println(clazz.getCanonicalName());
                 } catch (Exception e) {
                     e.printStackTrace();  
